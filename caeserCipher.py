ae23f2b0c9f35db02d97ffa029d0c9ae799b2bf1
cozysuman <sumanizrhythm@gmail.com>
2019-02-06 21:09:35 +0545

    Update caeserCipher.py

--- caeserCipher.py
+++ caeserCipher.py
@@ -6,54 +6,38 @@ def getMode():
 	else:
 		print('enter either "encrypt" "e" "decrypt" "d"')
 
-
 def plainText():
 	return (input('Enter your message '))
-
 
 def getKey():
 	key=int(input('enter key (1-26)'))
 	return key
 
-
 def cipherText(mode,message,key):
 	if mode[0]=='d':
 		key= -key
 	translated=''
-
-
 	for symbol in message:
 		if symbol.isalpha():
 			num= ord(symbol)
 			num+=key
-
-
 			if symbol.isupper():
 				if num > ord('Z'):
 					num-=26
-
 				elif num<ord('A'):
 					num+=26
-
 			elif symbol.islower():
 				if num>ord('z'):
 					num-=26
 				elif num<ord('a'):
 					num+=26
-
 			translated+=chr(num)
-
 		else:
 			translated +=symbol
-
 	return translated
-
 
 mode=getMode()
 message=plainText()
 key=getKey()
-
-
-	
 
 print(cipherText(mode, message, key))
